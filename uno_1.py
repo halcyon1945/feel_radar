64943c8f2b9efdbd8c436d93c7eb84967a30cadc
Suntaro.S <shuntaro.shinohara@gmail.com>
2020-12-16 09:01:34 +0900

    change if to FM24-NP100
    serial check and log

--- uno_1.py
+++ uno_1.py
@@ -1,13 +1,14 @@
 import serial
 import binascii
 import time
-ser=serial.Serial('/dev/serial0',115200,timeout=0.5)
-data_old=""
+ser = serial.Serial('/dev/serial0', 57600, bytesize=serial.EIGHTBITS,
+                    parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1.0)
+data_old = ""
 
-while 1 :
-    data=ser.readline()
-    if data_old != data:
-        print(data)
+while 1:
+    data = ser.readline()
+#    if data_old != data:
+    print(time.strftime("%a %b %d %H:%M:%S %Y", time.strptime(time.ctime())),data, "\n")
 
     data_old = data
     time.sleep(0.3)
