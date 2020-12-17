991b23deddde56cd1631148974c1724f35795485
Suntaro.S <shuntaro.shinohara@gmail.com>
2020-12-18 00:14:31 +0900

    distance calc impliment

--- uno_1.py
+++ uno_1.py
@@ -3,14 +3,14 @@ import binascii
 import time
 ser = serial.Serial('/dev/serial0', 57600, bytesize=serial.EIGHTBITS,
                     parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1.0)
-data_old = ""
+buf=['0','0','0','0','0']
 
 while 1:
-    data = ser.readline()
-#    if data_old != data:
-    print(time.strftime("%a %b %d %H:%M:%S %Y", time.strptime(time.ctime())),data, "\n")
-
-    data_old = data
-    time.sleep(0.3)
+    for num in range(4,0,-1):
+        buf[num]=buf[num-1]
+    buf[0] = ser.read()
+    if buf[4] == b'\xff' and buf[3] == b'\xff' and buf[2] == b'\xff':
+        distance = int.from_bytes(buf[1],'little')*256+int.from_bytes(buf[0],'little')
+        print(time.strftime("%a %b %d %H:%M:%S %Y", time.strptime(time.ctime())),int.from_bytes(buf[1],'little'),int.from_bytes(buf[0],'little'),distance, "\n")
 
 ser.close()
