99fc7b431ebfb6c5a23de6c9b03f516ab9be907f
Suntaro.S <shuntaro.shinohara@gmail.com>
2021-01-05 16:49:19 +0900

    change log at test

--- src/radar_laser_main.py
+++ src/radar_laser_main.py
@@ -31,10 +31,12 @@ def main():
             buf[num]=buf[num-1]
         buf[0] = ser.read()
         if buf[4] == b'\xff' and buf[3] == b'\xff' and buf[2] == b'\xff':
+            distance_prev=distance
             distance = (int.from_bytes(buf[1],'little')*256+int.from_bytes(buf[0],'little'))*10
             #print(time.strftime("%a %b %d %H:%M:%S %Y", time.strptime(time.ctime())),int.from_bytes(buf[1],'little'),int.from_bytes(buf[0],'little'),distance, "\n")
-            print(time.strftime("%a %b %d %H:%M:%S %Y :", time.strptime(time.ctime())),distance," mm")
-        show_l(distance,numDevices,HeliosLib)
+            if round(distance/100) != round(distance_prev/100):
+                print(time.strftime("%a %b %d %H:%M:%S %Y :", time.strptime(time.ctime())),distance," mm")
+                show_l(distance,numDevices,HeliosLib)
     
     ser.close()
     HeliosLib.CloseDevices()
@@ -44,11 +46,11 @@ def main():
 
 def show_l(dist_y,numDevices,HeliosLib):
     # actual lenth per laser base lines 
-    act_y=np.array([500 ,1000    ,2000      ,6000      ,10000])
+    act_y=np.array([  300,   301 ,     303,      1200  ,9000])
     las_y=np.array([  0 ,1023.75 ,1023.75*2 ,1023.75*3 ,1023.75*4])
 
     frames = [0 for x in range(1)]
-    frameType = HeliosPoint * 1000
+    frameType = HeliosPoint * 100
     inc100=int(0xFFF)/99
     scr_y = -1
 
@@ -56,18 +58,19 @@ def show_l(dist_y,numDevices,HeliosLib):
         scr_y = -1
     else:
         scr_y = round(  np.interp(dist_y,act_y,las_y))
-        #print(dist_y)
+        #print(dist_y,scr_y)
 
         frames[0] = frameType()
-        for i in range(100):
-            frames[0][i] = HeliosPoint(int(inc100 * i), int(scr_y),0x0,0xFF,0x0,0xFF)
+        for i in range(80):
+            frames[0][i] = HeliosPoint(int(inc100 * i + inc100 *20), int(scr_y),0x0,0xFF,0x0,0xFF)
 
         for j in range(numDevices):
             statusAttempts = 0
             # Make 512 attempts for DAC status to be ready. After that, just give up and try to write the frame anyway
             while (statusAttempts < 512 and HeliosLib.GetStatus(j) != 1):
                 statusAttempts += 1
-            HeliosLib.WriteFrame(j, int(50000), 0, ctypes.pointer(frames[0]), 100)   #Send the frame
+            HeliosLib.WriteFrame(j, int(30000), 1, ctypes.pointer(frames[0]), 80)   #Send the frame
+
 
     return
 
